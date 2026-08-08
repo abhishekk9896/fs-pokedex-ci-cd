c7ab75e2f37bfb1a7af0bc680448f89d487a3e02
abhishek9896 <abhishekrjvr@gmail.com>
2026-08-08 22:22:01 +0530

    Add version and health endpoints with executable build script

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
+// Dummy endpoint to verify new deployments
+app.get('/version', (req, res) => {
+  res.send('1') // Increment this string to verify new deployed versions
+})
+
+// Application health check endpoint for Render
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 const start = async () => {
   await app.listen(PORT)
   console.log(`server started on port ${PORT}`)
